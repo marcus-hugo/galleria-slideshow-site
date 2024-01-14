2a5f6e12acb2b2c61e63efeb801ea4d8fa1e99fb
marcushugo <phantomboss300@yahoo.com>
2024-01-14 09:24:41 -0600

    set port number to process.env.PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const server = express()
 const path = require("path")
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 const paintings = require("./paintings.js")
 let start
